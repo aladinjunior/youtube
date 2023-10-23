3bd0afbfe9c9c188d512b4e939765d4f7b1a804c
Aladin Júnior <aladinjr013@icloud.com>
2023-10-23 13:29:15 -0300

    shadow overlay added

--- app/src/main/java/co/aladinjunior/youtube/main/view/MainActivity.kt
+++ app/src/main/java/co/aladinjunior/youtube/main/view/MainActivity.kt
@@ -3,9 +3,11 @@ package co.aladinjunior.youtube.main.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.aladinjunior.youtube.R
 import co.aladinjunior.youtube.databinding.ActivityMainBinding
+import co.aladinjunior.youtube.databinding.VideoDetailedBinding
 import co.aladinjunior.youtube.main.data.API
 import co.aladinjunior.youtube.main.model.Video
 import kotlinx.coroutines.*
@@ -18,21 +20,25 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     override var coroutineContext: CoroutineContext = Dispatchers.IO + job
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var videoDetailedBinding: VideoDetailedBinding
     private lateinit var adapter: VideoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        videoDetailedBinding = VideoDetailedBinding.bind(binding.root)
+
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.title = ""
 
         val videos: MutableList<Video> = mutableListOf()
         adapter = VideoAdapter(videos) {
-
+            showOverlayView()
         }
         binding.mainRv.layoutManager = LinearLayoutManager(this)
         binding.mainRv.adapter = adapter
+
 
 
         launch {
@@ -54,6 +60,29 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
 
 
+    }
+
+    private fun showOverlayView() {
+        videoDetailedBinding.viewLayer.animate().apply {
+            duration = 400
+            alpha(0.5f)
+        }
+
+        binding.motionContainer.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                if (p3 > 0.5f) videoDetailedBinding.viewLayer.alpha = 1.0f - p3
+                else videoDetailedBinding.viewLayer.alpha = 0.5f
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
